75a4c893a06330e0b5d0314f5fd7fe05650fe531
Noor Alam <nooralamjaturi@gmail.com>
2023-07-09 18:54:14 -0500

    fixed issue

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :address        => ENV['smtp.mailgun.org'],
-  :port           => ENV['587'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :port           => ENV['MAILGUN_SMTP_PORT'],
   :authentication => :plain,
-  :user_name      => ENV['postmaster@sandbox4aef5b4631944389a9f557d8ac5272f4.mailgun.org'],
-  :password       => ENV['8a16dd0c9092e3e043a4a4a4365cccac-6d8d428c-3eb447d0'],
-  :domain         => ENV['sandbox4aef5b4631944389a9f557d8ac5272f4.mailgun.org'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => ENV['MAILGUN_DOMAIN'],
   :enable_starttls_auto => true
 }
-ActionMailer::Base.default_url_options = { host: ENV['localhost:3000'] }
+ActionMailer::Base.default_url_options = { host: ENV['URL'] }
